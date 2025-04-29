a11085580781b466dc7e1dd7e83445708d856edf
AlvaroValdez <v.alvaro.r@gmail.com>
2025-04-28 22:57:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const StellarSdk = require('stellar-sdk');
-
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(bodyParser.json());
-
 app.post('/firmar-challenge', async (req, res) => {
   try {
     const { transaction } = req.body;
-
     if (!process.env.SECRET_KEY) {
       throw new Error("SECRET_KEY is not defined");
     }
-
     if (!transaction) {
       return res.status(400).json({ error: 'Falta el parámetro: transaction' });
     }
-
     const keypair = StellarSdk.Keypair.fromSecret(process.env.SECRET_KEY);
-
     // Parseamos el challenge recibido
     const tx = new StellarSdk.Transaction(transaction, StellarSdk.Networks.TESTNET);
     //const tx = StellarSdk.TransactionBuilder.fromXDR(transaction, StellarSdk.Networks.TESTNET);
@@ -34,7 +27,6 @@ app.post('/firmar-challenge', async (req, res) => {
     res.status(500).json({ error: 'Error al firmar el challenge' });
   }
 });
-
 app.listen(port, () => {
   console.log(`Microservicio SEP-10 activo en puerto ${port}`);
 });
